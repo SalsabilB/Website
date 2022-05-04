cf4486e30f6285b41f591263eb57774a1efb119f
SalsabilB <bahijesalsabil@gmail.com>
2022-05-04 02:19:23 +0200

    header v1.1

---
+++ home.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home</title>
+
+    <!--Swiper CSS link-->
+    <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css">
+    <!--Font Awsome CDN link-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <!--Custom CSS file link-->
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+
+    <!--Header Section Starts-->
+    <section class="header">
+
+        <a href="home.php" class="logo">travel.</a>
+
+        <nav class="navbar">
+            <a href="home.php">home</a>
+            <a href="about.php">about</a>
+            <a href="package.php">package</a>
+            <a href="book.php">book</a>
+        </nav>
+
+        <div id="menu-btn" class="fas fa-bars"></div>
+
+    </section>
+    <!--Header Section Ends :) -->
+</body>
+</html>
